f090b82e7eb3f3e5533b5c5119a1908617dc77a3
Abner Lima <abnerlima.1997@gmail.com>
2023-08-19 14:20:21 -0300

    eslint and prettier config

--- front-end/.eslintrc.cjs
+++ front-end/.eslintrc.cjs
@@ -1,18 +1,50 @@
 module.exports = {
   root: true,
   env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["react-refresh", "functional"],
+  extends: ["eslint:recommended"],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
+    "react-refresh/only-export-components": [
+      "warn",
       { allowConstantExport: true },
     ],
   },
-}
+  overrides: [
+    {
+      files: ["*.ts", "*.tsx"],
+      extends: [
+        "airbnb",
+        "airbnb/hooks",
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:react-hooks/recommended",
+        "plugin:functional/external-vanilla-recommended",
+        "plugin:functional/recommended",
+        "plugin:functional/stylistic",
+        "prettier",
+      ],
+      parserOptions: {
+        project: ["./tsconfig.json", "./tsconfig.node.json"],
+      },
+      rules: {
+        "react/react-in-jsx-scope": "off",
+        "react/jsx-filename-extension": [
+          "error",
+          { extensions: [".jsx", ".tsx"] },
+        ],
+        "functional/no-expression-statements": "off",
+        "functional/functional-parameters": "off",
+        "functional/no-try-statements": "error",
+        "functional/no-this-expressions": "error",
+        "@typescript-eslint/switch-exhaustiveness-check": "error",
+        "no-var": "error",
+        "no-param-reassign": "error",
+        "prefer-const": "error",
+        "quotes": ["error", "single"],
+        "semi": ["error", "always"],
+      },
+    },
+  ],
+};
